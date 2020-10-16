8fd7a66b9cd5a7f36c4f4f1c3b67c940a177645a
Mauricio Vásquez <mauricio@kinvolk.io>
2020-10-16 17:18:52 -0500

    Injectfd experiment

--- contrib/cmd/seccompagent/seccompagent.go
+++ contrib/cmd/seccompagent/seccompagent.go
@@ -17,6 +17,56 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+
+/*
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <seccomp.h>
+#include <linux/seccomp.h>
+#include <sys/ioctl.h>
+
+struct _mydef_seccomp_notif_addfd {
+  __u64 id;
+  __u32 flags;
+  __u32 srcfd;
+  __u32 newfd;
+  __u32 newfd_flags;
+};
+
+#define SECCOMP_IOC_MAGIC		'!'
+#define SECCOMP_IO(nr)			_IO(SECCOMP_IOC_MAGIC, nr)
+#define SECCOMP_IOR(nr, type)		_IOR(SECCOMP_IOC_MAGIC, nr, type)
+#define SECCOMP_IOW(nr, type)		_IOW(SECCOMP_IOC_MAGIC, nr, type)
+#define SECCOMP_IOWR(nr, type)		_IOWR(SECCOMP_IOC_MAGIC, nr, type)
+
+#ifndef SECCOMP_ADDFD_FLAG_SETFD
+#define SECCOMP_ADDFD_FLAG_SETFD (1UL << 0)
+#endif
+
+#ifndef SECCOMP_IOCTL_NOTIF_ADDFD
+#define SECCOMP_IOCTL_NOTIF_ADDFD                                              \
+  SECCOMP_IOW(3, struct _mydef_seccomp_notif_addfd)
+#endif
+
+int replace_fd(__u64 id, int notify_fd, int fd) {
+	struct _mydef_seccomp_notif_addfd addfd = {
+		.id = id,
+		.flags = 0,
+		.srcfd = fd,
+		.newfd = 0,
+		.newfd_flags = 0,
+	};
+	int ret = ioctl(notify_fd, SECCOMP_IOCTL_NOTIF_ADDFD, &addfd);
+
+	printf("newfd is %d\n", addfd.newfd);
+
+	printf("return code of ioctl is %d\n", ret);
+	return ret;
+}
+
+*/
+import "C"
+
 var (
 	socketFile string
 )
@@ -105,12 +155,24 @@ func runMkdirForContainer(pid uint32, fileName string, mode uint32) error {
 	return nil
 }
 
+func runOpenForContainer() int {
+	fd, err := syscall.Open("/tmp/devnull2", 0, 0)
+	if err != nil {
+		return -1
+	}
+
+	return fd
+}
+
 // notifHandler handles seccomp notifications and responses
 func notifHandler(fd libseccomp.ScmpFd) {
 	defer syscall.Close(int(fd))
 	for {
 		req, err := libseccomp.NotifReceive(fd)
 		if err != nil {
+			if err == syscall.ECANCELED{
+				continue
+			}
 			fmt.Printf("Error in NotifReceive(): %s", err)
 			return
 		}
@@ -119,7 +181,7 @@ func notifHandler(fd libseccomp.ScmpFd) {
 			fmt.Printf("Error in decoding syscall %v(): %s", req.Data.Syscall, err)
 			return
 		}
-		fmt.Printf("Received syscall %q, pid %v, arch %q, args %+v\n", syscallName, req.Pid, req.Data.Arch, req.Data.Args)
+		//fmt.Printf("Received syscall %q, pid %v, arch %q, args %+v\n", syscallName, req.Pid, req.Data.Arch, req.Data.Args)
 
 		// TOCTOU check
 		if err := libseccomp.NotifIDValid(fd, req.ID); err != nil {
@@ -139,11 +201,13 @@ func notifHandler(fd libseccomp.ScmpFd) {
 			fileName, err := readArgString(req.Pid, int64(req.Data.Args[0]))
 			if err != nil {
 				fmt.Printf("Cannot read argument: %s", err)
-			} else {
-				fmt.Printf("mkdir: %q\n", fileName)
 			}
+			//else {
+			//	fmt.Printf("mkdir: %q, mode: %d\n", fileName, uint32(req.Data.Args[1]))
+			//}
 			err = runMkdirForContainer(req.Pid, fileName, uint32(req.Data.Args[1]))
 			if err != nil {
+				fmt.Printf("Error in runMkdirForContainer: %s\n", err)
 				resp.Error = int32(syscall.ENOSYS)
 				resp.Val = ^uint64(0) // -1
 			}
@@ -152,11 +216,29 @@ func notifHandler(fd libseccomp.ScmpFd) {
 			resp.Error = int32(syscall.ENOMEDIUM)
 			resp.Val = ^uint64(0) // -1
 			resp.Flags = 0
+		case "openat":
+			fmt.Println("open executed")
+			fileName, err := readArgString(req.Pid, int64(req.Data.Args[1]))
+			if err != nil {
+				fmt.Printf("Cannot read argument: %s", err)
+			} else {
+				fmt.Printf("stat: %q\n", fileName)
+			}
+
+			if fileName == "/dev/null2" {
+				fileFd := runOpenForContainer()
+				if fileFd != - 1 {
+					ret := C.replace_fd(C.ulonglong(req.ID), C.int(fd), C.int(fileFd))
+					resp.Flags = 0
+					fmt.Printf("fd of file is %d\n", fileFd)
+					resp.Val = uint64(ret) // ?
+				}
+			}
 		}
 
 		if err = libseccomp.NotifRespond(fd, resp); err != nil {
 			fmt.Printf("Error in notification response: %s", err)
-			return
+			//return
 		}
 	}
 }
