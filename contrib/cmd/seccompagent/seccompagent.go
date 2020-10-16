d6dea61f2dbe27f5850ade7691f98dbe1817e834
Mauricio Vásquez <mauricio@kinvolk.io>
2020-10-16 17:18:52 -0500

    Injectfd experiment
    This commit modifies the seccompagent to how an example of the inject fd
    mechanishm.
    
    The agent injects an fd of a file in the host ("/tmp/devnull2") when the target
    process tries to open "/dev/null2".

--- contrib/cmd/seccompagent/seccompagent.go
+++ contrib/cmd/seccompagent/seccompagent.go
@@ -9,13 +9,54 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strings"
 	"syscall"
 
 	"github.com/opencontainers/runtime-spec/specs-go"
 	libseccomp "github.com/seccomp/libseccomp-golang"
 	"golang.org/x/sys/unix"
 )
+
+/*
+#include <stdio.h>
+#include <seccomp.h>
+#include <linux/seccomp.h>
+#include <sys/ioctl.h>
+
+struct _mydef_seccomp_notif_addfd {
+  __u64 id;
+  __u32 flags;
+  __u32 srcfd;
+  __u32 newfd;
+  __u32 newfd_flags;
+};
+
+#define SECCOMP_IOC_MAGIC		'!'
+#define SECCOMP_IO(nr)			_IO(SECCOMP_IOC_MAGIC, nr)
+#define SECCOMP_IOR(nr, type)	_IOR(SECCOMP_IOC_MAGIC, nr, type)
+#define SECCOMP_IOW(nr, type)	_IOW(SECCOMP_IOC_MAGIC, nr, type)
+#define SECCOMP_IOWR(nr, type)	_IOWR(SECCOMP_IOC_MAGIC, nr, type)
+
+#ifndef SECCOMP_ADDFD_FLAG_SETFD
+#define SECCOMP_ADDFD_FLAG_SETFD (1UL << 0)
+#endif
+
+#ifndef SECCOMP_IOCTL_NOTIF_ADDFD
+#define SECCOMP_IOCTL_NOTIF_ADDFD                                              \
+  SECCOMP_IOW(3, struct _mydef_seccomp_notif_addfd)
+#endif
+
+int replace_fd(__u64 id, int notify_fd, int fd) {
+	struct _mydef_seccomp_notif_addfd addfd = {
+		.id = id,
+		.flags = 0,
+		.srcfd = fd,
+		.newfd = 0,
+		.newfd_flags = 0,
+	};
+	return ioctl(notify_fd, SECCOMP_IOCTL_NOTIF_ADDFD, &addfd);
+}
+*/
+import "C"
 
 var (
 	socketFile string
@@ -24,6 +65,11 @@ var (
 func init() {
 	flag.StringVar(&socketFile, "socketfile", "/run/seccomp-agent.socket", "Socket file")
 }
+
+const (
+	fileNameContainer = "/dev/null2"
+	fileNameHost      = "/tmp/devnull2"
+)
 
 func handleNewMessage(sockfd int) (*os.File, error) {
 	MaxNameLen := 4096
@@ -90,19 +136,28 @@ func readArgString(pid uint32, offset int64) (string, error) {
 	return string(s), nil
 }
 
-func runMkdirForContainer(pid uint32, fileName string, mode uint32) error {
-	if strings.HasPrefix(fileName, "/") {
-		err := syscall.Mkdir(fmt.Sprintf("/proc/%d/root%s-boo", pid, fileName), mode)
-		if err != nil {
-			return err
-		}
-	} else {
-		err := syscall.Mkdir(fmt.Sprintf("/proc/%d/cwd/%s-boo", pid, fileName), mode)
-		if err != nil {
-			return err
-		}
+func runOpenForContainer(fileName string, fd int,
+	req *libseccomp.ScmpNotifReq, resp *libseccomp.ScmpNotifResp) {
+	if fileName != fileNameContainer {
+		return
 	}
-	return nil
+
+	fileFd, err := syscall.Open(fileNameHost, 0, 0)
+	if err != nil {
+		fmt.Println("Failed to open file at host")
+		return
+	}
+	defer syscall.Close(fileFd)
+
+	ret := C.replace_fd(C.ulonglong(req.ID), C.int(fd), C.int(fileFd))
+	if ret < 0 {
+		fmt.Println("Error replacing fd on target")
+		return
+	}
+	resp.Flags = 0
+	resp.Val = uint64(ret)
+
+	fmt.Printf("Injected fd %d into target process for %q\n", ret, fileNameHost)
 }
 
 // notifHandler handles seccomp notifications and responses
@@ -111,15 +166,17 @@ func notifHandler(fd libseccomp.ScmpFd) {
 	for {
 		req, err := libseccomp.NotifReceive(fd)
 		if err != nil {
+			if err == syscall.ECANCELED {
+				continue
+			}
 			fmt.Printf("Error in NotifReceive(): %s", err)
 			return
 		}
 		syscallName, err := req.Data.Syscall.GetName()
 		if err != nil {
 			fmt.Printf("Error in decoding syscall %v(): %s", req.Data.Syscall, err)
-			return
+			continue
 		}
-		fmt.Printf("Received syscall %q, pid %v, arch %q, args %+v\n", syscallName, req.Pid, req.Data.Arch, req.Data.Args)
 
 		// TOCTOU check
 		if err := libseccomp.NotifIDValid(fd, req.ID); err != nil {
@@ -135,28 +192,25 @@ func notifHandler(fd libseccomp.ScmpFd) {
 		}
 
 		switch syscallName {
-		case "mkdir":
+		case "openat":
+			fileName, err := readArgString(req.Pid, int64(req.Data.Args[1]))
+			if err != nil {
+				fmt.Printf("Cannot read argument: %s", err)
+				goto notify
+			}
+			runOpenForContainer(fileName, int(fd), req, resp)
+		case "open":
 			fileName, err := readArgString(req.Pid, int64(req.Data.Args[0]))
 			if err != nil {
 				fmt.Printf("Cannot read argument: %s", err)
-			} else {
-				fmt.Printf("mkdir: %q\n", fileName)
+				goto notify
 			}
-			err = runMkdirForContainer(req.Pid, fileName, uint32(req.Data.Args[1]))
-			if err != nil {
-				resp.Error = int32(syscall.ENOSYS)
-				resp.Val = ^uint64(0) // -1
-			}
-			resp.Flags = 0
-		case "chmod":
-			resp.Error = int32(syscall.ENOMEDIUM)
-			resp.Val = ^uint64(0) // -1
-			resp.Flags = 0
+			runOpenForContainer(fileName, int(fd), req, resp)
 		}
 
+	notify:
 		if err = libseccomp.NotifRespond(fd, resp); err != nil {
 			fmt.Printf("Error in notification response: %s", err)
-			return
 		}
 	}
 }
@@ -178,6 +232,8 @@ func main() {
 		panic(fmt.Errorf("cannot listen on %s: %s", socketFile, err))
 	}
 	defer l.Close()
+
+	fmt.Println("Waiting for seccomp fds")
 
 	for {
 		conn, err := l.Accept()
